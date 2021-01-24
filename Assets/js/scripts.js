a5c7d6fb16063eba22ad8c96eab2ed2f99ff7c8b
Marcio Garozzo <mgarozzo@est.frba.utn.edu.ar>
2021-01-24 05:19:47 -0300

    Se personaliza carousel y se agrega seccion de tendencias alimentada automaticamente por la API de peliculas/series

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -5,6 +5,8 @@ const WRAPPERPELICULAS = document.getElementById('wrapperPeliculas');
 let peliculaJSON;
 let peliculas;
 
+/* https://api.themoviedb.org/3/trending/all/week?api_key=ab5eea38d623f059c3196ac7fb88a4c1 Obtener Tendencias */
+/* https://api.themoviedb.org/3/discover/movie?api_key=ab5eea38d623f059c3196ac7fb88a4c1&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1 Peliculas Populares*/
 function obtenerPeliculas(url,callback) {
     var response;
     response = new XMLHttpRequest();
@@ -22,6 +24,11 @@ function renderizarPeliculas(respuesta) {
     peliculaJSON = JSON.parse(respuesta.responseText);
     peliculas = peliculaJSON.results;
     crearPeliculaHTML(peliculas);
+}
+function renderizarTendencias(respuesta) {
+    peliculaJSON = JSON.parse(respuesta.responseText);
+    peliculas = peliculaJSON.results;
+    crearTendencias(peliculas);
 }
 
 function crearPeliculaHTML(coleccionPeliculas) {
@@ -56,4 +63,45 @@ function crearPeliculaHTML(coleccionPeliculas) {
         sinopsis.innerHTML = unaPelicula.overview;
         cardBody.appendChild(sinopsis);
     }
+}
+
+function crearTendencias(coleccionPeliculas) {
+    for (let unaPelicula of coleccionPeliculas) {
+        /* Creo nueva col */
+        let nuevaColumna = document.createElement('div')
+        nuevaColumna.classList.add('col');
+        WRAPPERPELICULAS.appendChild(nuevaColumna);
+        /* Creo card */
+        let nuevaCard = document.createElement('div');
+        nuevaCard.classList.add('card');
+        nuevaCard.classList.add('h-100');
+        nuevaCard.classList.add('shadow-sm');
+        nuevaColumna.appendChild(nuevaCard);
+        /* Agrego imagen de la noticia */
+        let imagen = document.createElement('img');
+        imagen.src = `${BASEURLIMG}${unaPelicula.poster_path}`;
+        imagen.classList.add('card-img-top');
+        nuevaCard.appendChild(imagen);
+        /* Agrego card-body */
+        let cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        nuevaCard.appendChild(cardBody);
+        /* Agrego titulo noticia */
+        let tituloPelicula = document.createElement('h3');
+        tituloPelicula.classList.add('card-title');
+        if(unaPelicula.media_type == "tv")
+            tituloPelicula.innerHTML = unaPelicula.name;
+        else
+            tituloPelicula.innerHTML = unaPelicula.title;
+        cardBody.appendChild(tituloPelicula);
+        /* Agrego descripcion noticia */
+        let sinopsis = document.createElement('p');
+        sinopsis.classList.add('card-text');
+        sinopsis.innerHTML = unaPelicula.overview;
+        cardBody.appendChild(sinopsis);
+    }
+}
+
+function cargarTendencias(){
+    obtenerPeliculas(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}`,renderizarTendencias);
 }
