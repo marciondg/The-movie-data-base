be29a11b0893ec3c314dd695ae59799345cc2e06
Lucia Rizzo <luciarizzodev@gmail.com>
2021-01-27 17:44:53 -0300

    form contact en las secciones peliculas y series

---
+++ Assets/js/contacto.js
@@ -0,0 +1,51 @@
+/*Form*/
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("form1");
+    /*var button = document.getElementById("my-form-button");*/
+    var status = document.getElementById("my-form-status");
+    var modal = document.getElementById("primerModal");
+    var modalEnviado = document.getElementById("modalEnviar");
+    var modalError = document.getElementById("modalError");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+        form.reset();
+        modal.style = "display: none ";
+        /*status.innerHTML = "Thanks!";*/
+        modalEnviado.style = "display: block ";
+    }
+
+    function error() {
+        modal.style = "display: none ";
+        modalError.style = "display: block ";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
